95a3e1ac8acfbcd23fb796d5d175a7883bcbdc4c
ccgrinsted <ccgrinsted@gmail.com>
2026-09-05 09:39:50 -0700

    Fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['fs', 'path', 'child_process'],
+    },
+  },
+  optimizeDeps: {
+    include: ['html2pdf.js'],
+  },
+});
